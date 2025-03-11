c27ded72965ef84c3e3965e0b9e6c7aa4d3c7527
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-03-11 09:03:24 +0600

    first

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("HELL"),
+        
+        ),
+        body: const Center(
+          child: 
+        Text("Welcome to HELL after mid!"),)
+      ),
+
+    );
+  }
+}

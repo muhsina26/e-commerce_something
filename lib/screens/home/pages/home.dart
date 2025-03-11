d34a9d943e0a0c3d6797d2f0c24805dbedf93051
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-03-11 10:35:55 +0600

    NAVBAR

--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:lab7/utils/const/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,13 +9,31 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Mega Shop",
-        style: TextStyle(
-          fontFamily: "DMSans",
-          fontWeight: FontWeight.bold
+        title: Text(
+          "Mega Shop",
+          style: TextStyle(
+            fontFamily: "DMSans",
+            fontWeight: FontWeight.bold,
+            color: FColors.oceanBlue,
+          ),
         ),
-        
-        ),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Icon
+            (Iconsax.notification_bing_outline,
+            color: FColors.darkGrey,),
+            ),
+            Container(
+            padding: EdgeInsets.all(10),
+            child: Icon
+            (Iconsax.notification_bing_outline,
+            color: FColors.darkGrey,),
+            ),
+      
+              
+          
+        ],
         centerTitle: true,
       ),
     );

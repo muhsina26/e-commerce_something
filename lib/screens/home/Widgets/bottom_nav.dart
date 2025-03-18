bd80a57661257adc59418f74ccddfa774183b021
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-03-18 10:17:42 +0600

    seperate_bottom_nav

---
+++ lib/screens/home/Widgets/bottom_nav.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart'; 
+
+class FBottomBar extends StatelessWidget {
+  const FBottomBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTapMethod,
+  });
+
+  final int currentIndex;
+  final Function(int) onTapMethod;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: currentIndex,
+      onTap: onTapMethod,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Iconsax.home_outline),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Iconsax.heart_outline),
+          label: "Wishlist",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Iconsax.shopping_bag_outline),
+          label: "Order",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Iconsax.profile_add_outline),
+          label: "Login",
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:lab7/screens/home/Widgets/bottom_nav.dart';
 
 import '../../../common/widgets/app_bar.dart';
+import '../Widgets/bottom_nav.dart'; 
 
-class Home extends StatelessWidget {
+ // Correct import
+
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    Center(child: Text("Home Screen")),
+    Center(child: Text("Wishlist Screen")),
+    Center(child: Text("Order Screen")),
+    Center(child: Text("Login Screen")),
+  ];
+
+  void onTapMethod(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +36,13 @@ class Home extends StatelessWidget {
       appBar: FAppBar(
         title: "Mega Shop",
         firsticon: Iconsax.notification_bing_outline,
-        //backButton: true,
+        // backButton: true,
+      ),
+      body: _screens[_currentIndex], // Display the selected screen
+      bottomNavigationBar: FBottomBar(
+        // Using the custom bottom nav bar
+        currentIndex: _currentIndex,
+        onTapMethod: onTapMethod,
       ),
     );
   }
